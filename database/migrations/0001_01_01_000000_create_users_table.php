5189f441a32e103e61067ce8d81afff2decef4ee
= <=>
2025-01-21 19:29:25 +0600

    Update default user_type in users table migration to 'basic'

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -27,9 +27,9 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['admin', 'client','doctor'])->default('client');
-            $table->enum('user_type', ['basic', 'premium','vip'])->default('normal');
+            $table->enum('user_type', ['basic', 'premium','vip'])->default('basic');
             $table->string('avatar')->nullable();
-            $table->boolean('agree_to_terms')->default(false); // Field to store agreement to terms
+            $table->boolean('agree_to_terms')->default(false); 
             $table->boolean('verified_registered')->default(false);
             $table->rememberToken();
             $table->timestamps();
