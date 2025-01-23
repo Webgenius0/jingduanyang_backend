d77f430387eaf2eb68ba33a8ba0c3cfc38876b62
= <=>
2025-01-23 10:13:12 +0600

    Add database seeders for orders and order products using Faker

---
+++ database/seeders/OrderProductSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class OrderProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+    */
+
+
+
+    public function run(): void
+    {
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 100; $i++) {
+            \App\Models\OrderPuduct::create([
+                'order_id' => $faker->numberBetween(1, 100),
+                'product_id' => $faker->uuid,
+                'name' => $faker->name,
+                'quantity' => $faker->numberBetween(1, 10),
+                'price' => $faker->randomFloat(2, 10, 100),
+                'currency' => $faker->randomElement(['USD', 'EUR', 'GBP']),
+                'description' => $faker->sentence,
+            ]);
+        }
+    }
+}
